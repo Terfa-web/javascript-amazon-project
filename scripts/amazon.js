10dda4efe884a72fe6bbc6af7a08cb475b0bb131
Nomhwange Terfa <afathelion@gmail.com>
2023-06-24 21:03:23 +0300

    Make teh cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -83,9 +83,14 @@ forEach((button) => {
       })
     }
 
-      
-    
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
    
-    console.log(cart);
   });
 });
